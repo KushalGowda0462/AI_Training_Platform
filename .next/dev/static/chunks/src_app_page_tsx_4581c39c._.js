b277adde4f33b13b5a0677cf5603cea0d9011b5d
Kushal Gowda K <KushalGowdaK@Kirans-MacBook-Pro.local>
2026-03-11 16:01:56 +0530

    changed gitignore

--- .next/dev/static/chunks/src_app_page_tsx_4581c39c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_aa0ab119._.js"
-],
-    source: "dynamic"
-});
